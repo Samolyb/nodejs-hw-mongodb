c6dd347e53ac3b538d2b63693fc8941d9ffc67bc
Samolyb <artemte6575@gmail.com>
2024-09-06 21:22:21 +0100

    update-server.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,20 @@
 import express from 'express';
-import mongoose from 'mongoose';
+
 import contactsRouter from './routers/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
-const app = express();
-app.use(express.json());
+export const startServer = () => {
+    const app = express();
+    app.use(express.json());
 
-app.use('/contacts', contactsRouter);
+    app.use('/contacts', contactsRouter);
 
-app.use(notFoundHandler);
-app.use(errorHandler);
+    app.use(notFoundHandler);
+    app.use(errorHandler);
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('Database connection successful');
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });
-    })
-    .catch(err => {
-        console.error('Database connection failed:', err.message);
-        process.exit(1);
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
+}; 
