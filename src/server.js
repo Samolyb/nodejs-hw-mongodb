5e3c7942f2f6076e39fca718e9350a2f944fa69f
Samolyb <artemte6575@gmail.com>
2024-09-11 22:55:29 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
-import contactsRouter from './routers/contacts.js';
-import errorHandler from './middlewares/errorHandler.js';
-import notFoundHandler from './middlewares/notFoundHandler.js';
+import { env } from "./utils/env.js";
+
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import logger from "../src/middlewares/logger.js";
+
+import contactsRouter from "./routers/contacts.js";
 
 export const startServer = () => {
     const app = express();
+
+    app.use(logger);
+    app.use(cors());
     app.use(express.json());
 
-    app.use('/contacts', contactsRouter);
+    app.use("/contacts", contactsRouter);
 
     app.use(notFoundHandler);
+
     app.use(errorHandler);
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}; 
+    const port = Number(env("PORT", 3000));
+
+    app.listen(port, () => console.log("Server running on port 3000"));
+};
