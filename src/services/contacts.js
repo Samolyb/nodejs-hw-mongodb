36f92780fec1216c6d7dd308ed0a8d41e1500bee
Samolyb <artemte6575@gmail.com>
2024-09-22 00:02:59 +0100

    Update-contacts

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,40 +1,46 @@
 import ContactCollection from "../db/models/сontacts.js";
-
 import sortFields from "../utils/parseSortParams.js";
-
-
 // import calculatePaginationData from '../utils/calculatePaginationData.js';
-// import { SORT_ORDER } from '../constants/index.js';
+import { SORT_ORDER } from '../constants/index.js';
 
 export const getAllContacts = async ({ perPage = 10, page = 1, sortBy = 'name', sortOrder = 'asc', filter = {} }) => {
-    const skip = (page - 1) * perPage;
+    try {
+        const skip = (page - 1) * perPage;
 
-    if (!sortFields.includes(sortBy)) {
-        throw new Error(`Invalid sort field: ${sortBy}`);
+        if (!sortFields.includes(sortBy)) {
+            throw new Error(`Invalid sort field: ${sortBy}`);
+        }
+
+        if (!SORT_ORDER.includes(sortOrder)) {
+            throw new Error(`Invalid sort order: ${sortOrder}. Use 'asc' or 'desc'.`);
+        }
+
+        const sortDirection = sortOrder === 'desc' ? -1 : 1;
+
+        const contacts = await ContactCollection.find(filter)
+            .skip(skip)
+            .limit(perPage)
+            .sort({ [sortBy]: sortDirection });
+
+
+        const count = await ContactCollection.countDocuments(filter);
+
+        const totalPages = Math.ceil(count / perPage);
+        const hasPreviousPage = page > 1;
+        const hasNextPage = page < totalPages;
+
+        return {
+            contacts,
+            page,
+            perPage,
+            totalItems: count,
+            totalPages,
+            hasPreviousPage,
+            hasNextPage,
+        };
+    } catch (error) {
+        throw new Error(`Failed to retrieve contacts: ${error.message}`);
     }
-
-    const sortDirection = sortOrder === 'desc' ? -1 : 1;
-
-    const contacts = await ContactCollection.find(filter)
-        .skip(skip)
-        .limit(perPage)
-        .sort({ [sortBy]: sortDirection });
-
-    const count = await ContactCollection.countDocuments(filter);
-
-    const totalPages = Math.ceil(count / perPage);
-    const hasPreviousPage = page > 1;
-    const hasNextPage = page < totalPages;
-
-    return {
-        contacts,
-        page,
-        perPage,
-        totalItems: count,
-        totalPages,
-        hasPreviousPage,
-        hasNextPage,
-    };
 };
 export const getContactById = async (id) => {
     const contacts = await ContactCollection.findById(id);
