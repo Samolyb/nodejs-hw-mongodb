67b42cff60b9cdca305e26f5dc2c06999e30afe9
Samolyb <artemte6575@gmail.com>
2024-09-23 19:39:53 +0100

    Update-calculate

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -5,8 +5,8 @@ const calculatePaginationData = ({ count, perPage, page }) => {
 
     return {
         totalPages,
-        hasNextPage,
         hasPreviousPage,
+        hasNextPage,
     };
 };
 
