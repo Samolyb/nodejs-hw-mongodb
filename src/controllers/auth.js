576bc76a0b06991919f0a9408cb498baa2855326
Samolyb <artemte6575@gmail.com>
2024-09-26 20:58:01 +0100

    Update auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -12,13 +12,11 @@ const setupSession = (res, session) => {
     });
 };
 
-
 export const registerController = async (req, res) => {
     const newUser = await authServices.register(req.body);
-
     res.status(201).json({
         status: 201,
-        message: "Succsessfully register user",
+        message: "Successfully registered a user!",
         data: newUser,
     });
 };
@@ -26,19 +24,11 @@ export const registerController = async (req, res) => {
 export const loginController = async (req, res) => {
     const session = await authServices.login(req.body);
 
-    res.cookie("refreshToken", session.refreshToken, {
-        httpOnly: true,
-        expire: new Date(Date.now() + session.refreshTokenValidUntil),
-    });
-
-    res.cookie("sessionId", session._id, {
-        httpOnly: true,
-        expire: new Date(Date.now() + session.refreshTokenValidUntil),
-    });
+    setupSession(res, session);
 
     res.json({
         status: 200,
-        message: "Successfully login",
+        message: "Successfully logged in an user!",
         data: {
             accessToken: session.accessToken,
         }
@@ -53,7 +43,7 @@ export const refreshController = async (req, res) => {
 
     res.json({
         status: 200,
-        message: "Successfully refresh session",
+        message: "Successfully refreshed a session!",
         data: {
             accessToken: session.accessToken,
         }
