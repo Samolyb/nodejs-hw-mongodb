91befe2b16e83526c700ba1c50c10b820b902589
Samolyb <artemte6575@gmail.com>
2024-10-13 23:52:34 +0100

    Update

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { contactTypeList } from "../constans/contacts.js";
+import { contactTypeList } from "../constants/contacts.js";
 
 export const createStudentSchema = Joi.object({
     name: Joi.string().min(3).max(20).required().messages({
